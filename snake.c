2d4693cd0e5976b6803501f9908eed6ce8c5c92d
Sujith Atesh <sujithatesh@gmail.com>
2025-01-05 23:14:38 +0530

    Mostly done some finishing touches left

--- snake.c
+++ snake.c
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 
 #define TILE_SIZE 20
+#define SCREEN_WIDTH 600
+#define SCREEN_HEIGHT 600
+
 int speed = 2;
 
 typedef enum dir{
@@ -40,16 +43,16 @@ void addSnake(snake* head){
     head->next = head1;
     switch(dir){
         case W:
-            head1->rect = (Rectangle){head_x, head_y, TILE_SIZE, TILE_SIZE};
+            head1->rect = (Rectangle){1000, 1000, TILE_SIZE, TILE_SIZE};
             break;
         case A:
-            head1->rect = (Rectangle){head_x, head_y, TILE_SIZE, TILE_SIZE};
+            head1->rect = (Rectangle){1000, 1000, TILE_SIZE, TILE_SIZE};
             break;
         case S:
-            head1->rect = (Rectangle){head_x, head_y, TILE_SIZE, TILE_SIZE};
+            head1->rect = (Rectangle){1000, 1000, TILE_SIZE, TILE_SIZE};
             break;
         case D:
-            head1->rect = (Rectangle){head_x, head_y, TILE_SIZE, TILE_SIZE};
+            head1->rect = (Rectangle){1000, 1000, TILE_SIZE, TILE_SIZE};
             break;
         default:
             assert("FIXME!: Head doesn't have a direction");
@@ -72,6 +75,7 @@ int main(void){
     float speedMultiplier = 0;
     char scoreBuff[10] = "0";
     int score = 0;
+    bool GameOver = false;
 
     time_t seedTime = time(NULL);
     SetRandomSeed((int)seedTime);
@@ -88,105 +92,116 @@ int main(void){
     addSnake(&head);
 
     int test = 0;
-    InitWindow(600,600, "RAYLIB");
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake");
     float timer = 0.0f;
     while(!WindowShouldClose()){
         BeginDrawing();
         ClearBackground(RAYWHITE);
-        float deltaTime = GetFrameTime();
+        if(!GameOver){
+            float deltaTime = GetFrameTime();
 
-        sprintf(scoreBuff,"%d",score);
-        DrawText(scoreBuff, 600 - 20, 20, 20, RED);
+            sprintf(scoreBuff,"%d",score);
+            DrawText(scoreBuff, 600 - 20, 20, 20, RED);
 
-        // movement
-        timer += deltaTime;
-        if(timer > (1.0 / speed)){
-            timer = 0;
-            snake* temp = &head;
-            Rectangle prev = temp->rect;
-            Rectangle curr = temp->rect;
-            temp = temp->next;
-            while(temp != NULL){
-                curr = temp->rect;
-                temp->rect = prev;
-                prev = curr;
+            // movement
+            timer += deltaTime;
+            if(timer > (1.0 / speed)){
+                timer = 0;
+                snake* temp = &head;
+                Rectangle prev = temp->rect;
+                Rectangle curr = temp->rect;
                 temp = temp->next;
+                while(temp != NULL){
+                    if(CheckCollisionRecs(temp->rect, head.rect)){
+                        GameOver = true;
+                    }
+                    curr = temp->rect;
+                    temp->rect = prev;
+                    prev = curr;
+                    temp = temp->next;
+                }
+                switch(head.direction){
+                    case W:
+                        head.rect.y -= TILE_SIZE;
+                        break;
+                    case A:
+                        head.rect.x -= TILE_SIZE;
+                        break;
+                    case S:
+                        head.rect.y += TILE_SIZE;
+                        break;
+                    case D:
+                        head.rect.x += TILE_SIZE;
+                        break;
+                    default:
+                        assert("Init direction");
+                        break;
+                }
             }
-            switch(head.direction){
-                case W:
-                    head.rect.y -= TILE_SIZE;
-                    break;
-                case A:
-                    head.rect.x -= TILE_SIZE;
-                    break;
-                case S:
-                    head.rect.y += TILE_SIZE;
-                    break;
-                case D:
-                    head.rect.x += TILE_SIZE;
-                    break;
-                default:
-                    assert("Init direction");
-                    break;
+            if(IsKeyDown(KEY_W) && head.direction != S){
+                head.direction = W;
+                speed = 6 + speedMultiplier;
+            }
+            else if(IsKeyDown(KEY_A) && head.direction != D){
+                head.direction = A;
+                speed = 6 + speedMultiplier;
+            }
+            else if(IsKeyDown(KEY_S) && head.direction != W){
+                head.direction = S;
+                speed = 6 + speedMultiplier;
+            }
+            else if(IsKeyDown(KEY_D) && head.direction != A){
+                head.direction = D;
+                speed = 6 + speedMultiplier;
+            }
+
+            // reset speed
+            if(IsKeyReleased(KEY_W) || IsKeyReleased(KEY_S) || IsKeyReleased(KEY_D) || IsKeyReleased(KEY_A)){
+                speed = 2 + speedMultiplier;
+            }
+
+            //drawing
+            snake* temp = &head;
+            int counter = 0;
+            while(temp && (temp->rect.x >= 0 && temp->rect.x <= SCREEN_WIDTH) &&
+                           (temp->rect.y >= 0 && temp->rect.y <= SCREEN_WIDTH) ){
+                if(counter%2 == 0)
+                    DrawRectangleRec(temp->rect, RED);
+                else
+                    DrawRectangleRec(temp->rect, BLACK);
+                temp = temp->next;
+                counter++;
+            }
+
+            //check collision
+            if( CheckCollisionRecs(fruit, head.rect))
+            {
+                test++;
+                if(flag == true)
+                    addSnake(&head);
+                flag = false;
+                speedMultiplier += 0.2;
+                score++;
+            }
+
+            //go around
+            if(head.rect.x > 600) head.rect.x = 0;
+            if(head.rect.x < 0) head.rect.x = 600;
+            if(head.rect.y > 600) head.rect.y = 0;
+            if(head.rect.y < 0) head.rect.y = 600;
+
+            if(flag)
+                DrawRectangle(fruit.x, fruit.y, TILE_SIZE, TILE_SIZE, GREEN);
+            if(flag == false){
+                fruit.x = GetRandomValue(TILE_SIZE, SCREEN_WIDTH - TILE_SIZE);
+                fruit.y = GetRandomValue(TILE_SIZE, SCREEN_HEIGHT - TILE_SIZE);
+                flag = true;
             }
         }
-        if(IsKeyDown(KEY_W)){
-            head.direction = W;
-            speed = 6 + speedMultiplier;
-        }
-        else if(IsKeyDown(KEY_A)){
-            head.direction = A;
-            speed = 6 + speedMultiplier;
-        }
-        else if(IsKeyDown(KEY_S)){
-            head.direction = S;
-            speed = 6 + speedMultiplier;
-        }
-        else if(IsKeyDown(KEY_D)){
-            head.direction = D;
-            speed = 6 + speedMultiplier;
-        }
-
-        // reset speed
-        if(IsKeyReleased(KEY_W) || IsKeyReleased(KEY_S) || IsKeyReleased(KEY_D) || IsKeyReleased(KEY_A)){
-            speed = 2 + speedMultiplier;
-        }
-
-        //drawing
-        snake* temp = &head;
-        int counter = 0;
-        while(temp){
-            if(counter%2 == 0)
-                DrawRectangleRec(temp->rect, RED);
-            else
-                DrawRectangleRec(temp->rect, BLACK);
-            temp = temp->next;
-            counter++;
-        }
-
-        //check collision
-        if( CheckCollisionRecs(fruit, head.rect))
-        {
-            test++;
-            if(flag == true)
-                addSnake(&head);
-            flag = false;
-            speedMultiplier += 0.2;
-            score++;
-        }
-
-        //go around
-        if(head.rect.x > 600) head.rect.x = 0;
-        if(head.rect.x < 0) head.rect.x = 600;
-        if(head.rect.y > 600) head.rect.y = 0;
-        if(head.rect.y < 0) head.rect.y = 600;
-
-        if(flag)
-            DrawRectangle(fruit.x, fruit.y, TILE_SIZE, TILE_SIZE, GREEN);
-        if(flag == false){
-            fruit.x = GetRandomValue(TILE_SIZE, 600 - TILE_SIZE);
-            fruit.y = GetRandomValue(TILE_SIZE, 600 - TILE_SIZE);
-            flag = true;
+        else{
+            ClearBackground(RAYWHITE);
+            DrawText("Game Over!\n Score: ", SCREEN_WIDTH / 2 - 80, SCREEN_HEIGHT / 2, 20, RED);
+            DrawText(scoreBuff , SCREEN_WIDTH / 2 , SCREEN_HEIGHT / 2 + 22, 20, RED);
         }
 
         EndDrawing();
