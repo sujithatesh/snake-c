3e5220c00a7b6bd724cb4cc48ace4585743cd96e
sujithatesh <91463571+sujithatesh@users.noreply.github.com>
2025-01-05 23:46:25 +0530

    bro this might f up the whole thing

--- run.sh
+++ run.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sujith/gitshit/real-raylib/src
-gcc snake.c -Wall -g -o snake \
--L /home/sujith/gitshit/real-raylib/src \
--I /home/sujith/gitshit/real-raylib/src \
--lraylib -lm  \
-&& ./snake
+
+# Detect the operating system
+OS=$(uname)
+
+if [[ "$OS" == "Linux" ]]; then
+    echo "Building for Linux..."
+    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/sujith/gitshit/real-raylib/src
+    gcc snake.c -Wall -g -o snake \
+    -L /home/sujith/gitshit/real-raylib/src \
+    -I /home/sujith/gitshit/real-raylib/src \
+    -lraylib -lm \
+    && ./snake
+
+elif [[ "$OS" == "Darwin" ]]; then
+    echo "Building for macOS..."
+    gcc -v -Wall -Wextra -g snake.c -o snake \
+    -L /Users/sujith.varkala/C/raylib/raylib-5.5_macos/lib \
+    -I /Users/sujith.varkala/C/raylib/raylib-5.5_macos/include \
+    -lraylib -lm \
+    && export DYLD_LIBRARY_PATH=/Users/sujith.varkala/C/raylib/raylib-5.5_macos/lib:$DYLD_LIBRARY_PATH \
+    && ./snake
+
+else
+    echo "Unsupported OS: $OS"
+    exit 1
+fi
