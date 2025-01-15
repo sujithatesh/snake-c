798026cefef29550946caeebad402f491ac476ae
sujithatesh <91463571+sujithatesh@users.noreply.github.com>
2025-01-15 08:50:40 +0530

    Add files via upload
    tt

--- snake.c
+++ snake.c
@@ -1,137 +1,79 @@
+#include <stdio.h>
 #include <raylib.h>
 #include <stdlib.h>
-#include <assert.h>
-#include <stdio.h>
 #include <time.h>
-#include <stdbool.h>
 
 #define SCREEN_WIDTH 600
 #define SCREEN_HEIGHT 600
+
 #define TILE_SIZE 20
 
-enum Direction{
+enum Direction {
 	W = 0,
 	A,
 	S,
 	D
 };
-typedef enum Direction Direction;
 
-struct snake{
+typedef struct snake{
 	Rectangle rec;
 	Direction dir;
+	struct snake* prev;
 	struct snake* next;
-};
-typedef struct snake snake;
+}snake;
 
 snake* addSnake(snake* head){
 	snake* newHead = (snake*)malloc(sizeof(snake));
+	newHead->rec = head->rec;
 	newHead->next = head;
+	newHead->prev = NULL;
 	newHead->dir = head->dir;
-	newHead->rec.x = head->rec.x;
-	newHead->rec.y = head->rec.y;
-	switch(head->dir){
+	switch(newHead->dir){
 		case W:
-			newHead->rec.y -= TILE_SIZE;
 			break;
 		case A:
-			newHead->rec.x -= TILE_SIZE;
 			break;
 		case S:
-			newHead->rec.y += TILE_SIZE;
 			break;
 		case D:
-			newHead->rec.x += TILE_SIZE;
 			break;
-		default :
-			assert("Direction not set in addSnake");
 	}
 	return newHead;
 }
 
-
 int main(void){
-	int speed = 6;
-	bool flag = true;
-
-	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "snake");
 	snake* head = (snake*)malloc(sizeof(snake));
 	head->rec.x = 20;
 	head->rec.y = 20;
-	head->dir= D;
 	head->next = NULL;
-	for(int i = 0; i < 4; i++){
-		head = addSnake(head);
-	}
-
-	bool freeTheEnd = true;
+	head->prev = NULL;
+	head->dir = D;
 	SetRandomSeed(time(NULL));
-	Rectangle fruit;
-	fruit.x = 560;
-	fruit.y = 20;
+	double accumTime = 0;
 
-	float timePassed = 0;
+	InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Snake");
 	while(!WindowShouldClose()){
 		BeginDrawing();
 		ClearBackground(RAYWHITE);
 
-		flag = true;
-
-		// INPUT
+		accumTime += GetFrameTime();
+		//Keyboard movement: 
 		if(IsKeyDown(KEY_W)){
-			head->dir = W;
+			head->rec.y -= TILE_SIZE;
 		}
 		if(IsKeyDown(KEY_A)){
-			head->dir = A;
+			head->rec.x -= TILE_SIZE;
 		}
 		if(IsKeyDown(KEY_S)){
-			head->dir = S;
+			head->rec.y += TILE_SIZE;
 		}
 		if(IsKeyDown(KEY_D)){
-			head->dir = D;
+			head->rec.x += TILE_SIZE;
 		}
-
-		int counter = 0;
-		snake* temp = head;
-		while(temp->next != NULL){
-			temp = temp->next;
-			if(counter % 2)
-				DrawRectangle(temp->rec.x, temp->rec.y, 20, 20, RED);
-			else
-				DrawRectangle(temp->rec.x, temp->rec.y, 20, 20, BLACK);
-			counter++;
+		if(accumTime > 0.5){
+			accumTime = 0;
 		}
-
-		timePassed += GetFrameTime();
-		if(timePassed > 1.0f / speed){
-			timePassed = 0;
-
-				// perpetual motion
-			head = addSnake(head);
-			// if addSnake dont advance
-			if(freeTheEnd){
-				snake* iter = head;
-				while(iter->next->next != NULL){
-					iter = iter->next;
-				}
-				free(iter->next);
-				iter->next = NULL;
-			}
-			freeTheEnd = true;
-		}
-
-		DrawRectangle(fruit.x, fruit.y, TILE_SIZE, TILE_SIZE, GREEN);
-		if(fruit.x == head->rec.x && fruit.y == head->rec.y && flag){
-			flag = false;
-			addSnake(head);
-			int newX = GetRandomValue(TILE_SIZE, SCREEN_WIDTH);
-			int newY = GetRandomValue(TILE_SIZE, SCREEN_HEIGHT);
-			fruit.x = newX - newX % TILE_SIZE;
-			fruit.y = newY - newY % TILE_SIZE;
-			freeTheEnd = false;
-		}
-
-		DrawFPS(20,20);
+		DrawRectangle(head->rec.x, head->rec.y, TILE_SIZE, TILE_SIZE, RED);
 		EndDrawing();
 	}
 	return 0;
